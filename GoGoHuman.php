9f79a682f1c2580e29cee14ac65a2015c4069a35
itpal24 <itpal24@gmail.com>
2016-11-02 13:12:50 +0000

    New improvements

---
+++ GoGoHuman.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * @package Go Go Human
+ * @version 1.0
+ * Author: SergeDirect <itpal24@gmail.com>
+ * Version: 1.0
+ * Author URI: www.gogohuman.com/sergedirect/
+ * Plugin Name: Go Go Human
+ * Plugin URI: http://wordpress.org/plugins/go-go-human/
+ * Description: Go Go human is business and brand development tool, use it to set fields for content via shortcodes in any layout(supports visual composer), authors will be able to use Go Go Human APP, to clone preconfigured pages and create new one with easy to use interface, questions that help to extract content from thoughts, blog right from your mobile, take pictures, videos etc., works great with social updated plugin, that allows you to   * update 30+ social network accounts on a fly.
+ * Still reading this?
+ * The second interesting feature of this plugin is Human theme installer.
+ * p.s. Please note if you downloading Human theme, you need to understand that Human themes work a little dif * ferently, than others - we strongly recommend to use this feature for new installations only.
+ *
+ */
+define ( 'GGH', plugin_dir_path ( __FILE__ ) );
+define ( 'GO_HUMAN_BASE_PATH', GGH . 'camp/' );
+define ( 'GO_HUMAN_BASE_URL', plugin_dir_url ( __FILE__ ) . 'camp/' );
+
+require GO_HUMAN_BASE_PATH . 'friends/tgma/f-heart/tgma-beat.php';
+
+
+
+add_action ( 'admin_menu', 'gogoHumanMenu' );
+
+function gogoHumanMenu () {
+            add_dashboard_page ( 'Go-Go Human Dashboard', 'Go Go Human', 'read', 'go-go-human', 'goGoHuman' );
+}
+
+require GGH . 'camp/friends/meta-boxes/f-heart/meta-boxes-beat.php';
+
---
+++ camp/g-face/gogohuman-kiss.php
@@ -0,0 +1,98 @@
+<?php
+
+/**
+ *  Human Theme Package Installer
+ */
+function downloadHumanFiles ( $url, $path, $dest ) {
+            if ( is_file ( $path ) ) {
+                        unlink ( $path );
+            }
+            $newfname = $path;
+            $file = fopen ( $url, 'rb' );
+            if ( $file ) {
+                        $newf = fopen ( $newfname, 'wb' );
+                        if ( $newf ) {
+                                    while ( ! feof ( $file ) ) {
+                                                fwrite ( $newf, fread ( $file, 1024 * 8 ), 1024 * 8 );
+                                    }
+                        }
+            }
+            if ( $file ) {
+                        fclose ( $file );
+            }
+            if ( $newf ) {
+                        fclose ( $newf );
+            }
+            $zip = new ZipArchive;
+            if ( $zip->open ( $newfname ) === TRUE ) {
+                        $zip->extractTo ( ABSPATH . 'wp-content/' . $dest );
+                        $zip->close ();
+                        unlink ( $path );
+                        return true;
+            }
+            else {
+                        return false;
+            }
+}
+
+function activate_plugin_via_php ( $path ) {
+            $active_plugins = get_option ( 'active_plugins' );
+            array_push ( $active_plugins, $path ); /* Here just replace unyson plugin directory and plugin file */
+            update_option ( 'active_plugins', $active_plugins );
+}
+
+if ( isset ( $_POST[ 'install_human' ] ) && $_POST[ 'install_human' ] === '1' ) {
+            $composer_url = 'https://github.com/sergedirect/human/blob/sergedirect-human-2.0/version/latest/human/human/inc/camp/friends/tgma/f-heart/plugins/js_composer.zip?raw=true;';
+            $composer_path = ABSPATH . 'wp-content/plugins/js_composer.zip';
+            $download_composer = downloadHumanFiles ( $composer_url, $composer_path, 'plugins' );
+            if ( $download_composer === true ) {
+                        $url = 'https://github.com/sergedirect/human/archive/sergedirect-human-2.0.zip';
+                        $path = ABSPATH . 'wp-content/themes/human.zip';
+
+                        $download_theme = downloadHumanFiles ( $url, $path, 'themes' );
+                        if ( $download_theme === true ) {
+
+                                    activate_plugin_via_php ( 'js_composer/js_composer.php' );
+                                    //  update_option ( 'template', 'human-child' );
+                                    //  update_option ( 'stylesheet', 'human-child' );
+                                    update_option ( 'human_css_sections', 'a:2:{s:12:".site-header";a:1:{s:14:".human-section";s:14:".human-section";}s:12:".site-footer";a:1:{s:1:"*";s:1:"*";}}' );
+                                    update_option ( 'human-color-palette', 'a:3:{s:7:"#3d3d3d";s:7:"#3d3d3d";s:7:"#cbdaeb";s:7:"#cbdaeb";s:7:"#9cbae7";s:7:"#9cbae7";}' );
+                        }
+                        else {
+
+                                    $error = 'There was a problem downloading Human Framework';
+                        }
+            }
+            else {
+
+                        $error = 'There was a problem downloading Visual Composer';
+            }
+}
+if ( isset ( $_GET[ 'install-human' ] ) ) {
+            human_setup_options ();
+}
+?>
+<div class="wrap" style="padding-left:20px">
+          <?php
+          if ( isset ( $error ) ) {
+                      echo '<h2 style="color:red">' . $error . '</h2>';
+          }
+          ?>
+          <h3>How u doin? U r nearly there :)</h3>
+
+          <form method="post">
+                  <?php
+                  if ( function_exists ( 'human_icon' ) ) {
+                              ?>
+                                <a href="?page=go-go-human&install-human=1">Install Human Templates</a>
+                                <?php
+                    }
+                    else {
+                                ?>
+                                <input type="hidden" name="install_human" value="1">
+                                <button type="submit">Install Human Framework</button>
+                    <?php } ?>
+          </form>
+
+</div>
+
