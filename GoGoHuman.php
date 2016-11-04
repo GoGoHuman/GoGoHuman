607dbb703d23769e2b4fb8dc4b739d4fdf3e6d5b
itpal24 <itpal24@gmail.com>
2016-11-04 12:29:18 +0000

    Version Update

--- GoGoHuman.php
+++ GoGoHuman.php
@@ -2,12 +2,12 @@
 
 /**
  * @package Go Go Human
- * @version 1.0
+ * @version 1.2
  * Author: SergeDirect <itpal24@gmail.com>
- * Version: 1.0
+ * Version: 1.2
  * Author URI: www.gogohuman.com/sergedirect/
- * Plugin Name: Go Go Human
- * Plugin URI: http://wordpress.org/plugins/go-go-human/
+ * Plugin Name: GoGoHuman
+ * Plugin URI: http://gogohuman.com/
  * Description: Go Go human is business and brand development tool, use it to set fields for content via shortcodes in any layout(supports visual composer), authors will be able to use Go Go Human APP, to clone preconfigured pages and create new one with easy to use interface, questions that help to extract content from thoughts, blog right from your mobile, take pictures, videos etc., works great with social updated plugin, that allows you to   * update 30+ social network accounts on a fly.
  * Still reading this?
  * The second interesting feature of this plugin is Human theme installer.
